084e56ae3b591ef6c39364b6d965f87bf39416aa
BioHazard <3482991796@qq.com>
2024-03-11 22:09:32 +0800

    new file:   Week-2/exercise_10.8.py

---
+++ Week-2/exercise_10.8.py
@@ -0,0 +1,12 @@
+from string import ascii_uppercase
+
+p = 19
+q = 31
+public_key = (527, 589)
+cipher_text = "04926 44064 06010"
+
+phi_n = (p - 1) * (q - 1)
+e, n = public_key
+d = pow(e, -1, phi_n)
+plain_text = ''.join(ascii_uppercase[pow(int(group), d, n)] for group in ['049', '264', '406', '406', '010'])
+print(f"Plain text: {plain_text}")
