e7b92bfb4527a38865e21175e82559c24efcd592
BioHazard <3482991796@qq.com>
2024-03-11 22:13:14 +0800

    modified:   Week-2/exercise_10.8.py

--- Week-2/exercise_10.8.py
+++ Week-2/exercise_10.8.py
@@ -8,5 +8,8 @@ cipher_text = "04926 44064 06010"
 phi_n = (p - 1) * (q - 1)
 e, n = public_key
 d = pow(e, -1, phi_n)
-plain_text = ''.join(ascii_uppercase[pow(int(group), d, n)] for group in ['049', '264', '406', '406', '010'])
+plain_text = ''.join(ascii_uppercase[pow(int(group), d, n)]
+                     for group in ['049', '264', '406', '406', '010'])
 print(f"Plain text: {plain_text}")
+
+# >>> Plain text: HELLO
